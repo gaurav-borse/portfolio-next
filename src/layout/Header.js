b7f306aa0de66b3408db06dde3f9f806f4eec3fc
Gaurav Borse <gborse108@gmail.com>
2023-11-22 11:59:27 +0530

    Bug Resolved4.

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -10,11 +10,11 @@ const Header = ({ blog }) => {
   }, []);
   return (
     <Fragment>
-      <div className='mob-header'>
-        <div className='d-flex'>
-          <div className='navbar-brand'>
-            <Link href='/' legacyBehavior>
-              <a className='logo-text'>Sergio</a>
+      <div className="mob-header">
+        <div className="d-flex">
+          <div className="navbar-brand">
+            <Link href="/" legacyBehavior>
+              <a className="logo-text">Sergio</a>
             </Link>
           </div>
           <button
@@ -30,36 +30,35 @@ const Header = ({ blog }) => {
       {/* End Header */}
       {/* nav bar */}
       <header
-        className={`header-left ${
-          sideBarToggle ? "menu-open menu-open-desk" : ""
-        }`}
+        className={`header-left ${sideBarToggle ? "menu-open menu-open-desk" : ""
+          }`}
       >
-        <div className='scroll-bar'>
-          <div className='hl-top'>
-            <div className='hl-logo'>
-              <div className='img'>
-                <img src='static/img/profile.png' title='' alt='' />
+        <div className="scroll-bar">
+          <div className="hl-top">
+            <div className="hl-logo">
+              <div className="img">
+                <img src="static/img/profile.png" title="" alt="" />
               </div>
               <h5>Gaurav Borse</h5>
             </div>
           </div>
           {blog ? <MenuWithBlog /> : <MenuWithOutBlog />}
         </div>
-        <div className='nav justify-content-center social-icons'>
-          <a href='https://www.linkedin.com/in/gaurav-borse/' target='_blank'>
-            <i className='fab fa-linkedin-in' />
+        <div className="nav justify-content-center social-icons">
+          <a href="https://www.linkedin.com/in/gaurav-borse/" target="_blank" rel="noreferrer">
+            <i className="fab fa-linkedin-in" />
           </a>
-          <a href='https://github.com/gaurav-borse/' target='_blank'>
-            <i className='fab fa-github' />
+          <a href="https://github.com/gaurav-borse/" target="_blank" rel="noreferrer">
+            <i className="fab fa-github" />
           </a>
-          <a href='https://www.instagram.com/gaurav.borse_' target='_blank'>
-            <i className='fab fa-instagram' />
+          <a href="https://www.instagram.com/gaurav.borse_" target="_blank" rel="noreferrer">
+            <i className="fab fa-instagram" />
           </a>
-          <a href='https://dev.to/gauravborse' target='_blank'>
-            <i className='fab fa-dev' />
+          <a href="https://dev.to/gauravborse" target="_blank" rel="noreferrer">
+            <i className="fab fa-dev" />
           </a>
-          <a href='https://www.facebook.com/Gaurav.Borsee' target='_blank'>
-            <i className='fab fa-facebook-f' />
+          <a href="https://www.facebook.com/Gaurav.Borsee" target="_blank" rel="noreferrer">
+            <i className="fab fa-facebook-f" />
           </a>
         </div>
       </header>
@@ -70,52 +69,52 @@ export default Header;
 
 const MenuWithOutBlog = () => {
   return (
-    <ul className='nav nav-menu' id='pp-menu'>
-      <li data-menuanchor='home' className='active'>
-        <a className='nav-link' href='#home'>
-          <i className='ti-home' />
+    <ul className="nav nav-menu" id="pp-menu">
+      <li data-menuanchor="home" className="active">
+        <a className="nav-link" href="#home">
+          <i className="ti-home" />
           <span>Home</span>
         </a>
       </li>
-      <li data-menuanchor='about'>
-        <a className='nav-link' href='#about'>
-          <i className='ti-id-badge' />
+      <li data-menuanchor="about">
+        <a className="nav-link" href="#about">
+          <i className="ti-id-badge" />
           <span>About Me</span>
         </a>
       </li>
-      <li data-menuanchor='education'>
-        <a className='nav-link' href='#education'>
-          <i className='ti-book' />
+      <li data-menuanchor="education">
+        <a className="nav-link" href="#education">
+          <i className="ti-book" />
           <span>Edu. &amp; Skills</span>
         </a>
       </li>
-      <li data-menuanchor='experience'>
-        <a className='nav-link' href='#experience'>
-          <i className='ti-briefcase' />
+      <li data-menuanchor="experience">
+        <a className="nav-link" href="#experience">
+          <i className="ti-briefcase" />
           <span>Experience</span>
         </a>
       </li>
-      {/* <li data-menuanchor='services'>
-        <a className='nav-link' href='#services'>
-          <i className='ti-panel' />
+      {/* <li data-menuanchor="services">
+        <a className="nav-link" href="#services">
+          <i className="ti-panel" />
           <span>Services</span>
         </a>
       </li> */}
-      <li data-menuanchor='work'>
-        <a className='nav-link' href='#work'>
-          <i className='ti-desktop' />
+      <li data-menuanchor="work">
+        <a className="nav-link" href="#work">
+          <i className="ti-desktop" />
           <span>Projects</span>
         </a>
       </li>
-      {/* <li data-menuanchor='blog' className='blog'>
-        <a className='nav-link' href='#blog'>
-          <i className='ti-layout-media-overlay-alt-2' />
+      {/* <li data-menuanchor="blog" className="blog">
+        <a className="nav-link" href="#blog">
+          <i className="ti-layout-media-overlay-alt-2" />
           <span>Blogs</span>
         </a>
       </li> */}
-      <li data-menuanchor='contactus'>
-        <a className='nav-link' href='#contactus'>
-          <i className='ti-map-alt' />
+      <li data-menuanchor="contactus">
+        <a className="nav-link" href="#contactus">
+          <i className="ti-map-alt" />
           <span>Contact Me</span>
         </a>
       </li>
@@ -132,51 +131,51 @@ const MenuWithBlog = () => {
 
   return (
     <Fragment>
-      <ul className='nav nav-menu' id='pp-menu'>
-        <li data-menuanchor='home'>
-          <Link href='/#home' legacyBehavior>
-            <a className='nav-link'>
-              <i className='ti-home' />
+      <ul className="nav nav-menu" id="pp-menu">
+        <li data-menuanchor="home">
+          <Link href="/#home" legacyBehavior>
+            <a className="nav-link">
+              <i className="ti-home" />
               <span>Home</span>
             </a>
           </Link>
         </li>
-        <li data-menuanchor='about'>
-          <Link href='/#about' legacyBehavior>
-            <a className='nav-link'>
-              <i className='ti-id-badge' />
+        <li data-menuanchor="about">
+          <Link href="/#about" legacyBehavior>
+            <a className="nav-link">
+              <i className="ti-id-badge" />
               <span>About Me</span>
             </a>
           </Link>
         </li>
-        <li data-menuanchor='services'>
-          <Link href='/#services' legacyBehavior>
-            <a className='nav-link'>
-              <i className='ti-panel' />
+        <li data-menuanchor="services">
+          <Link href="/#services" legacyBehavior>
+            <a className="nav-link">
+              <i className="ti-panel" />
               <span>Services</span>
             </a>
           </Link>
         </li>
-        <li data-menuanchor='work'>
-          <Link href='/#work' legacyBehavior>
-            <a className='nav-link'>
-              <i className='ti-bookmark-alt' />
+        <li data-menuanchor="work">
+          <Link href="/#work" legacyBehavior>
+            <a className="nav-link">
+              <i className="ti-bookmark-alt" />
               <span>Portfolio</span>
             </a>
           </Link>
         </li>
-        <li data-menuanchor='blog' className='blog active'>
-          <Link href='/#blog' legacyBehavior>
-            <a className='nav-link'>
-              <i className='ti-layout-media-overlay-alt-2' />
+        <li data-menuanchor="blog" className="blog active">
+          <Link href="/#blog" legacyBehavior>
+            <a className="nav-link">
+              <i className="ti-layout-media-overlay-alt-2" />
               <span>Blogs</span>
             </a>
           </Link>
         </li>
-        <li data-menuanchor='contactus'>
-          <Link href='/#contactus' legacyBehavior>
-            <a className='nav-link'>
-              <i className='ti-map-alt' />
+        <li data-menuanchor="contactus">
+          <Link href="/#contactus" legacyBehavior>
+            <a className="nav-link">
+              <i className="ti-map-alt" />
               <span>Contact Me</span>
             </a>
           </Link>
