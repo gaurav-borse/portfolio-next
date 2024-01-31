04a500dc712bba4adcfe5cfe682bdaa5d99ef976
Gaurav Borse <46493526+gaurav-borse@users.noreply.github.com>
2024-01-31 18:20:08 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-EDEPH7MYT3"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag(){dataLayer.push(arguments)}
   gtag('js', new Date());
 
   gtag('config', 'G-EDEPH7MYT3');
